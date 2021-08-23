1a3aafe5567884e8ba0016373af931c25b8d7a60
Jason <jakuffler@gmail.com>
2021-08-23 12:58:35 -0400

    logging allParksData

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import ParksList from './ParksList';
 
 function App() {
+ const [allParks, setAllParks] = useState([])
+
+  useEffect(() => {
+
+    fetch('https://developer.nps.gov/api/v1/parks?limit=465&api_key=b1KSaT3gid8CxzHNe1SNYD6ZGN7aCcNdWWgbTVY9')
+    .then((r) => r.json())
+    .then(allParksData => setAllParks(allParksData))
+  }, [])
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {allParks.map((park) =>  park={park})}
+
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/ParksList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ParksList({park}) {
+
+    return(
+        <main>
+            <div>
+                {park}
+            </div>
+        </main>
+    )
+}
+
+export default ParksList
