2f43f8f9b6544487c0a2187f5ae4ae9645686736
Jason <jakuffler@gmail.com>
2021-08-24 09:51:37 -0400

    succesfully defined data as allParks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 
 import './App.css';
 import React, {useEffect, useState} from 'react';
-import ParksList from './ParksList';
+// import ParksList from './ParksList';
 //import ParkCard from './ParkCard';
 
 function App() {
- const [allParks, setAllParks] = useState([])
+const [allParks, setAllParks] = useState([])
 
   useEffect(() => {
 
     fetch('https://developer.nps.gov/api/v1/parks?limit=465&api_key=b1KSaT3gid8CxzHNe1SNYD6ZGN7aCcNdWWgbTVY9')
     .then((r) => r.json())
-    .then(allParksData => console.log(Object.keys(setAllParks(allParksData.data))))
+    .then(allParksData => setAllParks(allParksData.data))
+    
   }, [])
-
-  // park={allParks.data.map((park) => park.data)}
+ 
+  console.log(allParks)
 
   return (
-    <div className="App">
-       <header className="park-list">
+    <div >
+       {/* <header className="park-list">
            <section>
            <ParksList allParks={allParks}  />
            </section>
-       </header>
+       </header> */}
      </div>
     )
   }
