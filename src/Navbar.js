5e0641c7c0bd04143a8e82124ab07b52f8048db6
seneiya <seneiyacooke@gmail.com>
2021-08-27 11:34:06 -0400

    added page reload when clicking page name and toUpperCase to search

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,19 @@
 import * as ReactBootStrap from 'react-bootstrap';
 
 function Navbar({searchPark}){
+
+function reload (){
+  window.location.reload()
+}
+
   return (
     <ReactBootStrap.Navbar bg="success" expand="sm">
   <ReactBootStrap.Container>
-    <ReactBootStrap.Navbar.Brand href="localhost:3000"><strong> Park Xplor </strong></ReactBootStrap.Navbar.Brand>
+    <ReactBootStrap.Navbar.Brand href='' onClick={reload}><strong> Park Xplor </strong></ReactBootStrap.Navbar.Brand>
     <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
     <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
       <ReactBootStrap.Nav className="me-auto">
-        <ReactBootStrap.Nav.Link href="localhost:3000"><strong> Home </strong></ReactBootStrap.Nav.Link>
-        <ReactBootStrap.Nav.Link href=""> <strong>All Parks</strong></ReactBootStrap.Nav.Link>
+        <ReactBootStrap.Nav.Link href="" onClick={reload}> <strong>All Parks</strong></ReactBootStrap.Nav.Link>
             <ReactBootStrap.Form className="d-flex">
               <ReactBootStrap.FormControl
                 type="search"
@@ -17,9 +21,10 @@ function Navbar({searchPark}){
                 className="mr-2"
                 aria-label="Search"
                 maxLength='2'
-                onChange={(e) => searchPark(e.target.value)}
+                onChange={(e) => searchPark(e.target.value.toUpperCase(), 
+                  e.preventDefault())}
               />
-              <ReactBootStrap.Button variant="outline-light" type='submit' >Search</ReactBootStrap.Button>
+              <ReactBootStrap.Button variant="outline-light" type='submit'>Search</ReactBootStrap.Button>
     </ReactBootStrap.Form>
       </ReactBootStrap.Nav>
     </ReactBootStrap.Navbar.Collapse>
